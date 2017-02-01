8410a88a5b11811d77aef9fb356efb90ebc419d7
jvd2 <vandeventer@syd-ml-deventer.home>
2017-02-01 15:30:43 +1100

    imported project

---
+++ hello world.py
@@ -0,0 +1,13 @@
+# to confirm python version
+# import sys
+# print sys.version
+
+#set py3 to false if version is 2 
+#from sys import version_info
+#py3 = version_info[0] > 2
+
+print ("hello")
+print ("what is your name?")
+yourName = raw_input("here:")
+print ("finished collecting name")
+print (yourName)
